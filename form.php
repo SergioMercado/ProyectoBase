7ac815bb844ba7850904e7873a9ce939ffd11154
SergioMercado <SergioMercado@github.com>
2018-06-02 17:08:17 -0500

    Archivo form modificado
    agregados tablas y formularios

--- form.php
+++ form.php
@@ -1,19 +1,112 @@
 <!DOCTYPE html>
-<?php
-$con = mysql_connect("localhost","root","","php") or die ("Error");
-?>
 
 
 <html>
 <head>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Page Title</title>
+  <title>Proyecto Crud</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-  <script src="main.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+  
+  
 </head>
 <body>
+
+<div class="container">
+
+<form action="form.php" method="POST">
+  <div class="form-group">
+      <label for="usr">Usuario:</label>
+      <input type="text" class="form-control" name='usr' id="usr">
+  </div>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <input type="password" class="form-control" name='pwd' id="pwd">
+  </div>
+  <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" name='email' id="email">
+  </div>
   
+  <button type="submit" class="btn btn-primary" id="insert">Submit</button>
+</form>
+
+
+</div>
+
+<?php
+$con = mysqli_connect("localhost","root","","proyecto_crud") or die ("Error");
+//if(isset($_POST['insert'])){
+
+  $usuario = $_POST['usr'];
+  $password= $_POST['pwd'];
+  $email = $_POST['email'];
+
+
+  $insertar="INSERT INTO users VALUES ($usuario, $password, $email)";
+ mysqli_query($con,$insertar);
+
+  //if($ejecutar){
+    //echo "<h3>Usuario Ingresado Correctamente</h3>";
+  //}
+
+//}
+
+?>
+
+<br />
+
+
+<div class="container">
+
+<table class="table table-dark">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>User</th>
+        <th>Password</th>
+        <th>Email</th>
+        <th>Editar</th>
+        <th>Borrar</th>
+      </tr>
+    </thead>
+    
+    <?php
+    $consulta = "SELECT * FROM users";
+    $ejecutar = mysqli_query($con, $consulta);
+    $i=0;
+    
+    while($fila = mysqli_fetch_array ($ejecutar)){
+      $id = $fila['id'];
+      $usuario = $fila['usuario'];
+      $password = $fila['email'];
+      $email = $fila['email'];
+    
+      $i++;
+    ?>
+   
+    <tbody>
+      <tr>
+        <td><?php echo $id; ?></td>
+        <td><?php echo $usuario?></td>
+        <td><?php echo $password?></td>
+        <td><?php echo $email?></td>
+        <td><a href = "form.php?editar=<?php echo $id; ?>">Editar</a></td>
+        <td><a href = "form.php?borrar=<?php echo $id; ?>">Borrar</a></td>
+      </tr>
+      
+    </tbody>
+    <?php }?>
+
+  </table>
+
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+
 </body>
 </html>
